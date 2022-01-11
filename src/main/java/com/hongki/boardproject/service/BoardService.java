9e6b199cba69829064e3cd38cfc06bb75e261676
Hong-kee <ghdehfdl123@gmail.com>
2022-01-11 17:54:51 +0900

    2022.01.11 게시판 서비스 로직단

---
+++ src/main/java/com/hongki/boardproject/service/BoardService.java
@@ -0,0 +1,58 @@
+package com.hongki.boardproject.service;
+
+import com.hongki.boardproject.domain.board.Board;
+import com.hongki.boardproject.domain.board.BoardRepository;
+import com.hongki.boardproject.domain.user.User;
+import com.hongki.boardproject.dto.board.BoardSaveRequestDto;
+import com.hongki.boardproject.dto.board.BoardUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+    @Transactional
+    public Long save(BoardSaveRequestDto boardSaveRequestDto, User user) {
+        boardSaveRequestDto.setUser(user);
+        return boardRepository.save(boardSaveRequestDto.toEntity()).getId();
+    }
+
+    /**
+     * 글목록 로직
+     */
+    public List<Board> findAll() {
+        return boardRepository.findAll();
+    }
+
+    /**
+     * 글상세 로직
+     */
+    @Transactional(readOnly = true)
+    public Board detail(Long id) {
+        return boardRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 id가 없습니다. id=" + id));
+    }
+
+    /**
+     * 글삭제 로직
+     */
+    @Transactional
+    public void deleteById(Long id) {
+        boardRepository.deleteById(id);
+    }
+
+    /**
+     * 글수정 로직
+     */
+    @Transactional
+    public Long update(Long id, BoardUpdateRequestDto boardUpdateRequestDto) {
+        Board board = boardRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 id가 없습니다. id=" + id));
+        board.update(boardUpdateRequestDto.getTitle(), boardUpdateRequestDto.getContent());
+        return id;
+    }
+}
